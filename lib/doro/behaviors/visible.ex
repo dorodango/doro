0ad5c9f33e2fccae8256062b887efe3527b8d621
Ingar Shu <ingar@carbonfive.com>
2018-07-23 19:55:57 -0700

    Fix visible behavior

--- lib/doro/behaviors/visible.ex
+++ lib/doro/behaviors/visible.ex
@@ -7,16 +7,16 @@ defmodule Doro.Behaviors.Visible do
     player != object || Doro.Entity.named?(player, rest)
   end
 
+  interact("look", ~w(l), %Doro.Context{player: player, object: object}) do
+    send_to_player(player, first_person_description(object))
+    send_to_others(player, "#{definite(player)} looks at #{definite(object)} thoughtfully.")
+  end
+
   def first_person_description(entity) do
     "#{definite(entity)} #{entity[:description]}"
   end
 
   defp look_at(player, object) when player == object do
     send_to_player(player, "You #{object[:description]}")
-  end
-
-  defp look_at(player, object) do
-    send_to_player(player, first_person_description(object))
-    send_to_others(player, "#{definite(player)} looks at #{definite(object)} thoughtfully.")
   end
 end
